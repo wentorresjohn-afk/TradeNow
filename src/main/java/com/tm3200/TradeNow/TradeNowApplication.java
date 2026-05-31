fc3d644cba1a99a53e0f52b196c8d8abded9bc62
wentorresjohn-afk <wentorresjohn@gmail.com>
2026-05-30 18:01:12 -0600

    Primer commit

---
+++ src/main/java/com/tm3200/TradeNow/TradeNowApplication.java
@@ -0,0 +1,13 @@
+package com.tm3200.TradeNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tm3200/TradeNow/TradeNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm3200.TradeNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
