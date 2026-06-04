8c77ac7eda9c55c430762e8553ab0e938c755d95
mariasalas0212 <mariajsalas0212@gmail.com>
2026-06-04 08:47:53 -0600

    restaurar UserJpaRepository con versión de Sebas, porque se perdió con el merge de John

--- src/main/java/com/tm3200/TradeNow/Repository/UserJpaRepository.java
+++ src/main/java/com/tm3200/TradeNow/Repository/UserJpaRepository.java
@@ -1,5 +1,17 @@
 package com.tm3200.TradeNow.Repository;
 
-public interface UserJpaRepository
-{
+import com.tm3200.TradeNow.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+
 }
