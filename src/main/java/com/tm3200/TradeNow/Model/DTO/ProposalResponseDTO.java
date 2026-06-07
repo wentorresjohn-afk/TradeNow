a7bbd55daaa7efc51b2296b50ea04f58dd32de72
mariasalas0212 <mariajsalas0212@gmail.com>
2026-06-06 21:07:11 -0600

    módulo 3: propuestas de intercambio

---
+++ src/main/java/com/tm3200/TradeNow/Model/DTO/ProposalResponseDTO.java
@@ -0,0 +1,35 @@
+package com.tm3200.TradeNow.Model.DTO;
+
+import com.tm3200.TradeNow.Model.ProposalStatus;
+import jakarta.validation.constraints.NotNull;
+
+public class ProposalResponseDTO {
+    @NotNull(message = "Status is required/Se requiere estado")
+    private ProposalStatus status;
+    //este no lleva notnull porque es opcional, solo se necesita si hay una contra oferta
+    private Integer counterOfferedPublicationId;
+
+    public ProposalResponseDTO() {
+    }
+
+    public ProposalResponseDTO(ProposalStatus status, Integer counterOfferedPublicationId) {
+        this.status = status;
+        this.counterOfferedPublicationId = counterOfferedPublicationId;
+    }
+
+    public ProposalStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ProposalStatus status) {
+        this.status = status;
+    }
+
+    public Integer getCounterOfferedPublicationId() {
+        return counterOfferedPublicationId;
+    }
+
+    public void setCounterOfferedPublicationId(Integer counterOfferedPublicationId) {
+        this.counterOfferedPublicationId = counterOfferedPublicationId;
+    }
+}
--- src/main/java/com/tm3200/TradeNow/Repository/ProposalJpaRepository.java
+++ src/main/java/com/tm3200/TradeNow/Repository/ProposalJpaRepository.java
@@ -1,5 +1,6 @@
 package com.tm3200.TradeNow.Repository;
 
+import com.tm3200.TradeNow.Model.Posts;
 import com.tm3200.TradeNow.Model.Proposal;
 import com.tm3200.TradeNow.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +11,7 @@ import java.util.List;
 @Repository
 public interface ProposalJpaRepository extends JpaRepository<Proposal,Integer> {
 
-    List<Proposal> findByTargetPostsId(Posts targetPublicationId);
+    List<Proposal> findByTargetPublicationId(Posts targetPublicationId);
     List<Proposal> findBySenderId(User senderId);
 
 }
